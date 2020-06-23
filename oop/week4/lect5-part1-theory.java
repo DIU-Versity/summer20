efc1b7fe8ab6cc84421cf73844a3a54850cb63c9
Mehrab-Mohul <48277137+Mehrab-Mohul@users.noreply.github.com>
2020-06-23 10:58:54 +0600

    Update lect5-part1-theory.java

--- oop/week4/lect5-part1-theory.java
+++ oop/week4/lect5-part1-theory.java
@@ -10,3 +10,64 @@ class Programmer extends Employee {
         System.out.println("Nmae:" + obj.name + "\nSalary: " + obj.salary + "\nSkill: " + obj.skill);
     }
 }
+
+
+# Types of inheritance example
+/* ....
+    - Single inheritance
+    - Multi-level inheritance
+    - hierarchical inheritance
+    - Multiple inheritance (Not supported in Java)
+.... */
+
+#Single inheritance
+public class Employee {
+    String name = "Jhon";
+    float salary = 50000;
+}
+class Programmer extends Employee {
+    String skill = "Java, Python";
+    public static void main(String[] args) {
+        Programmer obj = new Programmer();
+        System.out.println("Nmae:" + obj.name + "\nSalary: " + obj.salary + "\nSkill: " + obj.skill);
+    }
+}
+
+#Multi-level inheritance
+public class Employee {
+    String name = "Jhon";
+    float salary = 50000;
+}
+class Programmer extends Employee {
+    String skill = "Java, Python";
+}
+
+class GameProgrammer extends Programmer {
+    String games = "PUBG, Freefire";
+    public static void main(String[] args) {
+        GameProgrammer obj = new GameProgrammer();
+        System.out.println("Nmae:" + obj.name + "\nSalary: " + obj.salary + "\nSkill: " + obj.skill);
+        System.out.println("Developed game: " + obj.games);
+    }
+}
+
+# hierarchical inheritance
+public class Employee {
+    String name = "Jhon";
+    float salary = 50000;
+}
+class Programmer extends Employee {
+    String skill = "Java, Python";
+    public static void main(String[] args) {
+        Programmer obj = new Programmer();
+        System.out.println("Nmae:" + obj.name + "\nSalary: " + obj.salary + "\nSkill: " + obj.skill);
+    }
+}
+
+class Banker extends Employee {
+    String branch = "Dhanmondi";
+    public static void main(String[] args) {
+        Banker obj = new Banker();
+        System.out.println("Nmae:" + obj.name + "\nSalary: " + obj.salary + "\nBranch: " + obj.branch);
+    }
+}
